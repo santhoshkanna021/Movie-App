2f977818bb98f87b60120580bd6d096a9a45a18d
santhoshkanna021 <santhoshkannar802@gmail.com>
2025-09-10 10:23:22 +0530

    add the backend url

--- client/src/components/PopularSection.jsx
+++ client/src/components/PopularSection.jsx
@@ -9,7 +9,7 @@ const PopularSection = () => {
   const trendingIds = ['t-1', 't-2', 't-3', 't-4', 't-5', 't-6'];
 
   useEffect(() => {
-    axios.get('https://movie-app-backend-9wvy.onrender.com/api/movies')
+    axios.get('https://movie-app-backend-mu.vercel.app/api/movies')
       .then((res) => {
         // ❌ Filter out the trending movies
         const nonTrending = res.data.filter(movie => !trendingIds.includes(movie.id));
@@ -18,6 +18,7 @@ const PopularSection = () => {
       .catch((err) => {
         console.error('Error fetching movies:', err);
       });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
